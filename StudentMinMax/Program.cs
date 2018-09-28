6fba6c26b0f369ec325a7a99e8aa5a647ab2b962
JaneWB <brownjannick@att.net>
2018-09-28 12:55:39 -0500

    CG 11-3 Student MinMax

--- StudentMinMax/Program.cs
+++ StudentMinMax/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace StudentMinMax
 {
@@ -6,7 +10,33 @@ namespace StudentMinMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader file = new StreamReader("C:\\Users\\brown\\source\\repos\\StudentData\\StudentData\\studentData.txt");
+
+            List<Student> students = new List<Student>();
+
+            string line;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                string[] items = line.Split(" ");
+                Student s = new Student(items[0]);
+
+                for (int i = 1; i < items.Length; i++)
+                {
+                    s.AddScore(Int32.Parse(items[i]));
+                }
+
+                students.Add(s);
+
+            }
+            foreach (Student item in students)
+            {
+              
+                Console.WriteLine(item.Name + " " + item.MinGrade() + " " + item.MaxGrade());
+
+            }
+            Console.ReadLine();
         }
     }
 }
+
